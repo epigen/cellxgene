eca0097668572bc30db3bfb5f92af18b402b0e34
Moritz Schaefer <mail@moritzs.de>
2023-12-23 22:09:38 +0100

    Adapt LLM calling to up to date single-cellm state

--- server/common/compute/llm_embeddings.py
+++ server/common/compute/llm_embeddings.py
@@ -4,15 +4,16 @@ import logging
 import pandas as pd
 from pandas.core.dtypes.dtypes import CategoricalDtype
 from single_cellm.jointemb.config import TranscriptomeTextDualEncoderConfig
+from single_cellm.jointemb.processing import TranscriptomeTextDualEncoderProcessor
 import torch
-from single_cellm.config import model_path_from_name
+from single_cellm.config import get_path
 
 from single_cellm.jointemb.lightning import TranscriptomeTextDualEncoderLightning
 from single_cellm.jointemb.geneformer_model import GeneformerTranscriptomeProcessor
 from transformers import AutoTokenizer
 from single_cellm.validation.zero_shot.functions import (
-    write_enrichr_terms_to_json,
     anndata_to_scored_keywords,
+    formatted_text_from_df,
 )
 
 logger = logging.getLogger(__name__)
@@ -24,48 +25,47 @@ parent_repo = Path(subprocess.check_output(["git", "rev-parse", "--show-toplevel
 with open(parent_repo / "config.yaml") as f:
     parent_config = yaml.safe_load(f)
 
-
 logging.info("Loading LLM embedding model...")
 # Model loading
 
+# TODO new model
 model_path = Path(
-    "~/single-cellm/results/jointEmbed/lightning_logging/JointEmbed_Training/9dwkh6rl/checkpoints/epoch=5-val_loss=2.71.ckpt"
+    "~/single-cellm/results/wandb_logging/JointEmbed_Training/5e0elqs5/checkpoints/epoch=1-val_loss=3.46.ckpt"
 ).expanduser()
 
 # model = TranscriptomeTextDualEncoderModel.from_pretrained(geneformer_biogpt_model_path).to(device)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-config_transcriptome = {"model_type": "geneformer"}
-config_text = {"model_type": "biogpt"}
+# config_transcriptome = {"model_type": "geneformer"}
+# config_text = {"model_type": "biogpt"}
 
-model_config = TranscriptomeTextDualEncoderConfig(
-    projection_dim=512,
-    logit_scale_init_value=2.6592,
-    transcriptome_config=config_transcriptome,
-    text_config=config_text,
-)
+# model_config = TranscriptomeTextDualEncoderConfig(
+#     projection_dim=512,
+#     logit_scale_init_value=2.6592,
+#     transcriptome_config=config_transcriptome,
+#     text_config=config_text,
+# )
 
 
-pl_model = TranscriptomeTextDualEncoderLightning(
-    model_config
-)  # .load_from_checkpoint(model_path, "geneformer", "microsoft/biogpt")  # TODO need to load flexibly
+# pl_model = TranscriptomeTextDualEncoderLightning(
+#     model_config
+# )
+pl_model = TranscriptomeTextDualEncoderLightning.load_from_checkpoint(model_path)
 pl_model.eval().to(device)
 
-tokenizer = AutoTokenizer.from_pretrained("microsoft/biogpt")
-# image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
-transcriptome_processor = GeneformerTranscriptomeProcessor(nproc=4, emb_label=parent_config["anndata_label_name"])
+processor = TranscriptomeTextDualEncoderProcessor(
+    pl_model.model.transcriptome_model.config.model_type, pl_model.model.text_model.config.model_type
+)
 
-logging.info("Preparing EnrichR terms...")
-terms_json_path = Path("~/projects/single-cellm/resources/enrichr_terms/terms.json").expanduser()
-if not terms_json_path.exists():
-    write_enrichr_terms_to_json(terms_json_path=terms_json_path)
-else:
-    logging.info("EnrichR terms already exist - skipping")
+tokenizer = processor.get_tokenizer()
+transcriptome_processor = processor.get_transcriptome_processor()
 
-logging.info("Preparing done")
+terms_json_path = get_path(["paths", "enrichr_terms_json"])
+assert (
+    terms_json_path.exists()
+), "EnrichR terms json file does not exist. Please run single_cellm/src/validation/zero_shot/write_enrichr_terms.py"
 
 logging.info("Loading done")
-# processor = TranscriptomeTextDualEncoderProcessor(transcriptome_processor, tokenizer)
 
 
 def llm_obs_to_text(adaptor, mask):
@@ -74,7 +74,7 @@ def llm_obs_to_text(adaptor, mask):
     Keyword types used for comparison are: (i) selected enrichR terms (see single_cellm.validation.zero_shot.functions.write_enrichr_terms_to_json) \
     and (ii) cell type annotations (currently all values in adata.obs.columns). For more info, see single_cellm.validation.zero_shot.functions.
     :param adaptor: DataAdaptor instance
-    :param mask: 
+    :param mask:
     :return:  dictionary  {text: }
     """
 
@@ -89,24 +89,23 @@ def llm_obs_to_text(adaptor, mask):
     obs_cols = [c for c, t in adaptor.data.obs.dtypes.items() if isinstance(t, CategoricalDtype)]
 
     # get top n keywords by similarity
-    top_n_text = anndata_to_scored_keywords(
-        expression=expression[mask],
+    similarity_scores_df = anndata_to_scored_keywords(
+        transcriptome_input=expression[mask],
         model=pl_model.model,
         terms_json_path=terms_json_path,
         transcriptome_processor=transcriptome_processor,
         text_tokenizer=tokenizer,
-        device=device,
         average_mode="cells",  # TODO test which method is better, "cells" or "embeddings"
-        chunk_size_text_emb_and_scoring=64,
-        n_top_per_term=5,
+        batch_size=64,
         obs_cols=obs_cols,  # Note that this might be confusing to users and we need to see whather this stays shall remain enabled
         score_norm_method="zscore",  # TODO test which method is better
-        return_mode="text",
     )
+
+    similarity_formatted_text = formatted_text_from_df(similarity_scores_df, n_top_per_term=5)
 
     # TODO Implement a caching mechanism (e.g. could be deployed along with the model)
 
-    return {"text": top_n_text}
+    return {"text": similarity_formatted_text}
 
 
 def llm_text_to_annotations(adaptor, text):
