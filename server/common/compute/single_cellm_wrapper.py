988ae476ee8e01f961609254a8f15e33fcda9b0d
Moritz Schaefer <mail@moritzs.de>
2024-01-15 10:10:52 +0100

    Use of precomputed CLIP embeddings to accelerate web UI
    - ...
    - replace `obs_cols` with `additional_text_dict`

--- server/common/compute/single_cellm_wrapper.py
+++ server/common/compute/single_cellm_wrapper.py
@@ -58,18 +58,27 @@ class SingleCeLLMWrapper:
         Preprocess data for LLM embeddings, making sure that subsequent API requests run fast
         """
         logging.info("Preprocessing data for LLM embeddings")
+        model_processed_data_path = get_path(
+            ["paths", "model_processed_dataset"], dataset="tabula_sapiens", model="f6fjywkb"
+        )  # TODO pass these two values dynamically
+        if model_processed_data_path.exists():
+            logging.info("Loading precomputed data")
+            self.model_processed_data = np.load(model_processed_data_path, allow_pickle=True)
+            # only retain the embeddings (to save RAM)
+            self.model_processed_data = {k: self.model_processed_data[k] for k in ["orig_ids", "transcriptome_embeds"]}
+        else:
+            self.model_processed_data = None
+            logging.info("Precomputed data file does not exist. Precompute using FrozenCachedModel")
+            # Make sure that all the zero-shot class terms are embedded
+            mask = np.zeros(adaptor.data.shape[0], dtype=bool)
+            mask[0] = True  # Generate mask with single element
+            self.llm_obs_to_text(adaptor, mask=mask)
 
-        # TODO replace this with loading of full_data.npz
-        # Make sure that all the zero-shot class terms are embedded
-        mask = np.zeros(adaptor.data.shape[0], dtype=bool)
-        mask[0] = True  # Generate mask with single element
-        self.llm_obs_to_text(adaptor, mask=mask)
+            # Embed all cells
+            self.llm_text_to_annotations(adaptor, text="test")
 
-        # Embed all cells
-        self.llm_text_to_annotations(adaptor, text="test")
-
-        # Store
-        self.pl_model.model.store_cache()
+            # Store
+            self.pl_model.model.store_cache()
 
     def llm_obs_to_text(self, adaptor, mask):
         """
@@ -80,27 +89,34 @@ class SingleCeLLMWrapper:
         :param mask:
         :return:  dictionary  {text: }
         """
-
         var_index_col_name = adaptor.get_schema()["annotations"]["var"]["index"]
         obs_index_col_name = adaptor.get_schema()["annotations"]["obs"]["index"]
 
-        # TODO try to load embeddings via npz
-
-        expression = adaptor.data.copy()
-        expression.var.index = adaptor.data.var[var_index_col_name].astype(str)
-        expression.obs.index = adaptor.data.obs[obs_index_col_name].astype(str)
+        if self.model_processed_data is not None:
+            selected_obs_ids = set(adaptor.data.obs[obs_index_col_name].astype(str)[mask])
+            selector = [str(orig_id) in selected_obs_ids for orig_id in self.model_processed_data["orig_ids"]]
+            transcriptomes = torch.from_numpy(self.model_processed_data["transcriptome_embeds"][selector])
+            transcriptomes = transcriptomes.to(self.pl_model.model.device)
+        else:
+            # Provide raw read counts, which will be processed by the model
+            transcriptomes = adaptor.data[mask].copy()
+            transcriptomes.var.index = adaptor.data.var[var_index_col_name].astype(str)
+            transcriptomes.obs.index = adaptor.data.obs[obs_index_col_name].astype(str)
 
         obs_cols = [c for c, t in adaptor.data.obs.dtypes.items() if isinstance(t, CategoricalDtype)]
+        additional_text_dict = {
+            obs_col: adaptor.data.obs[obs_col].astype(str).unique().tolist() for obs_col in obs_cols
+        }
 
         similarity_scores_df = anndata_to_scored_keywords(
-            transcriptome_input=expression[mask],
+            transcriptome_input=transcriptomes,
             model=self.pl_model.model,
             terms_json_path=self.terms_json_path,
             transcriptome_processor=self.transcriptome_processor,
             text_tokenizer=self.tokenizer,
             average_mode="embeddings",
             batch_size=64,
-            obs_cols=obs_cols,
+            additional_text_dict=additional_text_dict,
             score_norm_method="zscore",
         )
 
@@ -122,28 +138,33 @@ class SingleCeLLMWrapper:
         """
         Embed the given text into the LLM space and return the similarity of each cell to the text. The similarity will be used as new cell-level annotation
         """
-
         # Converts an obs index of "0", "1", ... to "TTTGCATGAGAGGC-1", ...
         obs_index_col_name = adaptor.get_schema()["annotations"]["obs"]["index"]
         var_index_col_name = adaptor.get_schema()["annotations"]["var"]["index"]
 
-        # TODO try to load embeddings via npz
+        obs_ids = adaptor.data.obs[obs_index_col_name].astype(str).values
+        obs_ids_lookup = {v: k for k, v in enumerate(obs_ids)}
+        if self.model_processed_data is not None:
+            # Do fast embeddings lookup (order matters, so we select by indices rather than by mask)
+            selector = [obs_ids_lookup[str(orig_id)] for orig_id in self.model_processed_data["orig_ids"]]
+            transcriptomes = torch.from_numpy(self.model_processed_data["transcriptome_embeds"][selector])
+            transcriptomes = transcriptomes.to(self.pl_model.model.device)
+        else:
+            # Provide raw read counts, which will be processed by the model
+            transcriptomes = adaptor.data.copy()  # NOTE copy is slow!
+            transcriptomes.var.index = adaptor.data.var[var_index_col_name]
+            transcriptomes.obs.index = adaptor.data.obs[obs_index_col_name].astype(str)
 
-        expression = adaptor.data.copy()
-        expression.var.index = adaptor.data.var[var_index_col_name]
-        expression.obs.index = adaptor.data.obs[obs_index_col_name].astype(str)
-
-        # TODO similarities is [nan, ...]
-        scores, transcriptome_annotations = score_text_vs_transcriptome_many_vs_many(
+        scores, _ = score_text_vs_transcriptome_many_vs_many(
             model=self.pl_model.model,
-            transcriptome_input=expression,
+            transcriptome_input=transcriptomes,
             text_list_or_text_embeds=[text],
             transcriptome_processor=self.transcriptome_processor,
             text_tokenizer=self.tokenizer,
             average_mode=None,
             batch_size=64,
             score_norm_method=None,
-            transcriptome_annotations=expression.obs.index,
+            transcriptome_annotations=obs_ids,
         )
 
         return pd.Series(scores.squeeze(0).cpu().detach())
