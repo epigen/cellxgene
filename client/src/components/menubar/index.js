93c6d8b61c18b0c8cebcd34e5f4f100c77c65fe8
Moritz Schaefer <mail@moritzs.de>
2024-05-02 07:45:31 +0200

    Revert "Disable clipping because it leads to weird errors"
    This reverts commit 74045ed1c96f3f5b64db0b3bb7ca29675318a66c.

--- client/src/components/menubar/index.js
+++ client/src/components/menubar/index.js
@@ -236,23 +236,22 @@ class MenuBar extends React.PureComponent {
           undoDisabled={undoDisabled}
           redoDisabled={redoDisabled}
         />
-        {/* currently fails unfortunately */}
-        {/* <Clip */}
-        {/*   pendingClipPercentiles={pendingClipPercentiles} */}
-        {/*   clipPercentileMin={clipPercentileMin} */}
-        {/*   clipPercentileMax={clipPercentileMax} */}
-        {/*   handleClipOpening={this.handleClipOpening} */}
-        {/*   handleClipClosing={this.handleClipClosing} */}
-        {/*   handleClipCommit={this.handleClipCommit} */}
-        {/*   isClipDisabled={this.isClipDisabled} */}
-        {/*   handleClipOnKeyPress={this.handleClipOnKeyPress} */}
-        {/*   handleClipPercentileMaxValueChange={ */}
-        {/*     this.handleClipPercentileMaxValueChange */}
-        {/*   } */}
-        {/*   handleClipPercentileMinValueChange={ */}
-        {/*     this.handleClipPercentileMinValueChange */}
-        {/*   } */}
-        {/* /> */}
+        <Clip
+          pendingClipPercentiles={pendingClipPercentiles}
+          clipPercentileMin={clipPercentileMin}
+          clipPercentileMax={clipPercentileMax}
+          handleClipOpening={this.handleClipOpening}
+          handleClipClosing={this.handleClipClosing}
+          handleClipCommit={this.handleClipCommit}
+          isClipDisabled={this.isClipDisabled}
+          handleClipOnKeyPress={this.handleClipOnKeyPress}
+          handleClipPercentileMaxValueChange={
+            this.handleClipPercentileMaxValueChange
+          }
+          handleClipPercentileMinValueChange={
+            this.handleClipPercentileMinValueChange
+          }
+        />
         <Tooltip
           content="When a category is colored by, show labels on the graph"
           position="bottom"
