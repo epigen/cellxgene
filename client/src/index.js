676911579efcd23ebd5cdb7d23ca928ff17ff84b
Moritz <mschaefer@cemm.at>
2025-04-18 06:35:12 +0000

    Initial code (to be tested with <url>#injected

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { FocusStyleManager } from "@blueprintjs/core";
+import thunk from "redux-thunk";
+import { annoMatrixGC } from "./middleware";
 
 import "./index.css";
 
 /* our code */
 import App from "./components/app";
 import store from "./reducers";
+
 
 FocusStyleManager.onlyShowFocusOnTabs();
 
